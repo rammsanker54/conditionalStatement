5c557f75dcf42226c97cb161aa7ba343f8f867ce
rammsanker <109334253+rammsanker54@users.noreply.github.com>
2022-09-24 11:45:33 +0530

    Add files via upload

---
+++ condition statement/condition.js
@@ -0,0 +1,71 @@
+
+
+////
+
+
+
+
+
+
+
+
+
+
+let a = 8;
+let b = 38;
+let c = 10;
+ if ((a<b) && (a<c)){
+ if (b<c){
+ console.log(a,b,c)}
+ else{console.log(a,c,b)}
+ }
+else if ((b<a)&&(b<c))
+{ if (a<c){console.log(b,a,c)
+  }else{
+console.log(b,c,a)}
+   }
+else if (c<a&&c<b)
+{if (a<b)
+{console.log(c,a,b)}
+else {
+console.log(c,b,a);}
+ }
+ 
+ if (a<b && a<c){
+ if (b<c){
+ console.log(a,b,c);}
+ else{console.log(a,c,b);}}
+ else if( (b<a && b<c)
+ {if (c<a) {console.log(b,c,a);}
+ else(console.log(b,a,c);}}
+ else if (c<a && c<b){
+ if (b<a){console.log(c,b,a);}
+ else{console.log(c,a,b);}}
+ 
+ 
+if (condition){
+if (condition){
+console.log();
+}
+else {
+console.log();
+}
+}
+
+
+let x = 1;
+while(x<20){
+while(x%2==0){
+console.log(x)}
+x++} 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
